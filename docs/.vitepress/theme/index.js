1b1bc37f16617b29ec7a5b2fd192828937d92bea
sanbuphy <physicoada@gmail.com>
2026-08-13 14:45:36 +0800

    warn readers during course rewrite

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,4 +1,12 @@
 import DefaultTheme from "vitepress/theme";
+import { h } from "vue";
+import MaintenanceBanner from "./MaintenanceBanner.vue";
 import "./style.css";
 
-export default DefaultTheme;
+export default {
+  extends: DefaultTheme,
+  Layout: () =>
+    h(DefaultTheme.Layout, null, {
+      "layout-top": () => h(MaintenanceBanner),
+    }),
+};
